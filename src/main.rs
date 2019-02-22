6ac67947fdfb96161a7701494c80477f300cbbfa
m-yahya <yahya_gis@hotmail.com>
2019-02-22 10:02:45 +0100

    complete code with comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+// loading crates
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+// main function
+
 fn main() {
-    println!("Hello, world!");
+    // generate random number
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
+
+    // start loop
+    loop {
+        // declare a placeholder to store the user input
+        let mut guess = String::new();
+        println!("Please enter guess!");
+        // read the user input
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        // convert the string into integer
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {}", guess);
+
+        // check input
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Guess is too low!"),
+            Ordering::Greater => println!("Guess is too high!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
